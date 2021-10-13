b2fbe62bfc18973eff3d01ad985a2d028c56d1af
+ <brandi.ude@gmail.com>
2021-10-13 12:28:20 -0400

    commit tree 008412b6ff7d010aacef3e6a672364f59c040932

---
+++ index.js
@@ -0,0 +1,13 @@
+let num1 = 100
+let num2 = 2000
+
+ const divide = function () {
+     return  num2 / num1;
+}
+
+let x = 2
+const square = x => x * x;
+
+let a = 3
+let b = 4
+const add = (a, b) => a + b;
